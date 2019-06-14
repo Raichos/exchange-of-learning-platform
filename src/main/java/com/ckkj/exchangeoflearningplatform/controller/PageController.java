f941b3ec4da38b1d34f2171b86f36c47a5dcc291
Rakers1024 <1139991371@qq.com>
2019-06-14 17:39:24 +0800

    登陆页面

--- src/main/java/com/ckkj/exchangeoflearningplatform/controller/PageController.java
+++ src/main/java/com/ckkj/exchangeoflearningplatform/controller/PageController.java
@@ -3,6 +3,11 @@ package com.ckkj.exchangeoflearningplatform.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 /**
  * @author lzh
